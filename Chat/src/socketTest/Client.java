935e749940c5a6adbf8cacaf5641f816000853d6
realmingyeong <minrud0310@gmail.com>
2019-04-09 21:21:14 +0900

    chat 수정

--- Chat/src/socketTest/Client.java
+++ Chat/src/socketTest/Client.java
@@ -11,37 +11,23 @@ public class Client {
 	public static void main(String[] args) {
 
 		try {// 서버 접속
-
 			Socket socket = new Socket("127.0.0.1", 9000);
 
 			// Server에 보낼 데이터
-
 			BufferedWriter bufWriter =
-
 					new BufferedWriter(new OutputStreamWriter(socket.getOutputStream()));
-
 			bufWriter.write("hello world");
-
 			bufWriter.newLine();
-
 			bufWriter.flush();
 
 			// Server가 보낸 데이터 출력
-
 			BufferedReader bufReader =
-
 					new BufferedReader(new InputStreamReader(socket.getInputStream()));
-
 			String message = bufReader.readLine();
-
 			System.out.println("Message : " + message);
-
 			socket.close();
-
 			bufReader.close();
-
 			bufWriter.close();
-
 		}catch (Exception e) {
 			e.printStackTrace();
 		}
